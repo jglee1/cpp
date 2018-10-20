8b1f9e948879fa807d5275a843d988fb68c3ba97
jglee1 <jglee1@gmail.com>
2018-10-20 02:58:24 -0700

    added pi.cpp

---
+++ pi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+constexpr double GetPi() { return 22.0 / 7; }
+constexpr double TwicePi() { return 2 * GetPi(); }
+
+int main()
+{
+	using namespace std;
+	const double pi = 22.0 / 7;
+
+	cout << "constant pi contains value " << pi << endl;
+	cout << "constexpr GetPi() returns value " << GetPi() << endl;
+	cout << "constexpr TwicePi() returns value " << TwicePi() << endl;
+	cout << "M_PI in <cmath> "<< M_PI << endl;
+
+	return 0;
+}
