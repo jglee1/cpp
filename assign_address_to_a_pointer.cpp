6b92607087eca2dcef6c23a6d733f5cb7a70f87a
jglee1 <jglee1@gmail.com>
2018-12-01 08:14:00 -0500

    adding more

---
+++ assign_address_to_a_pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int age = 30;
+  int* pointsToInt = &age; //pointer initialized to &age
+
+  //Displaying the value of pointer
+  cout<<"Integer age is at: "<< pointsToInt << endl;
+
+  return 0;
+}
+
